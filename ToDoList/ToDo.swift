9f4ee318285d5054c887aa3a07f857f6e77a1f0f
Caitlyn Widjaja <widjajacaitlyn@gmail.com>
2021-05-16 10:46:02 -0700

    Initial Commit

---
+++ ToDoList/ToDo.swift
@@ -0,0 +1,15 @@
+//
+//  ToDo.swift
+//  ToDoList
+//
+//  Created by Anna Woolridge on 5/10/20.
+//  Copyright © 2020 Anna Woolridge. All rights reserved.
+//
+
+import UIKit
+
+class ToDo {
+    var name = ""
+    var importance = false
+}
+
---
+++ ToDoList/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ToDoList
+//
+//  Created by Anna Woolridge on 5/10/20.
+//  Copyright © 2020 Anna Woolridge. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
